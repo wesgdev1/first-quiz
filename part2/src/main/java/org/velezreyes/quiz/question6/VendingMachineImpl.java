016558a735e1a40f7cbaf3beff40017d3ddb32e1
Welinton Suarez <wesgdeveloper@gmail.com>
2023-11-04 14:54:34 -0500

    update parte 2

--- part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
+++ part2/src/main/java/org/velezreyes/quiz/question6/VendingMachineImpl.java
@@ -5,7 +5,58 @@ public class VendingMachineImpl implements VendingMachine {
   public static VendingMachine getInstance() {
     
     return new VendingMachineImpl();
-
-
+    
   }
+
+   private boolean quarterInserted = false;
+
+    @Override
+    public void insertQuarter() {
+        quarterInserted = true;
+    }
+
+    @Override
+    public Drink pressButton(String name) throws NotEnoughMoneyException, UnknownDrinkException {
+        if (!quarterInserted) {
+            throw new NotEnoughMoneyException();
+        }
+
+        if ("ScottCola".equals(name)) {
+            quarterInserted = false;
+            return new ScottCola();
+        } else if ("KarenTea".equals(name)) {
+            if (quarterInserted) {
+                quarterInserted = false;
+                return new KarenTea();
+            } else {
+                throw new NotEnoughMoneyException();
+            }
+        } else {
+            throw new UnknownDrinkException();
+        }
+    }
+
+    private class ScottCola implements Drink {
+        @Override
+        public String getName() {
+            return "ScottCola";
+        }
+
+        @Override
+        public boolean isFizzy() {
+            return true;
+        }
+    }
+
+    private class KarenTea implements Drink {
+        @Override
+        public String getName() {
+            return "KarenTea";
+        }
+
+        @Override
+        public boolean isFizzy() {
+            return false;
+        }
+    }
 }
